141d63a452211f4d50c14524e0c6ecf210bf4f1d
arthurztt <artkikk1@gmail.com>
2024-04-16 16:34:24 -0300

    Adicionado pasta de testes extra-curso e o arquivo pedra papel e tesoura atualizado.

---
+++ Prática Extra-Cursos/treinoFor.php
@@ -0,0 +1,61 @@
+<?php
+
+$numerosTabuada = [1,2,3,4,5,6,7,8,9,10];
+
+for ($i = 0; $i <= 10; $i++){
+  echo $numerosTabuada[0] . " x " . $i . "= " . ($numerosTabuada[0] * $i) . PHP_EOL;
+}
+
+echo "_________________________________" . PHP_EOL;
+
+for ($i = 0; $i <= 10; $i++){
+  echo $numerosTabuada[1] . " x " . $i . "= " . ($numerosTabuada[1] * $i) . PHP_EOL;
+}
+
+echo "_________________________________" . PHP_EOL;
+
+for ($i = 0; $i <= 10; $i++){
+  echo $numerosTabuada[2] . " x " . $i . "= " . ($numerosTabuada[2] * $i) . PHP_EOL;
+}
+
+echo "_________________________________" . PHP_EOL;
+
+for ($i = 0; $i <= 10; $i++){
+  echo $numerosTabuada[3] . " x " . $i . "= " . ($numerosTabuada[3] * $i) . PHP_EOL;
+}
+
+echo "_________________________________" . PHP_EOL;
+
+for ($i = 0; $i <= 10; $i++){
+  echo $numerosTabuada[4] . " x " . $i . "= " . ($numerosTabuada[4] * $i) . PHP_EOL;
+}
+
+echo "_________________________________" . PHP_EOL;
+
+for ($i = 0; $i <= 10; $i++){
+  echo $numerosTabuada[5] . " x " . $i . "= " . ($numerosTabuada[5] * $i) . PHP_EOL;
+}
+
+echo "_________________________________" . PHP_EOL;
+
+for ($i = 0; $i <= 10; $i++){
+  echo $numerosTabuada[6] . " x " . $i . "= " . ($numerosTabuada[6] * $i) . PHP_EOL;
+}
+
+echo "_________________________________" . PHP_EOL;
+
+for ($i = 0; $i <= 10; $i++){
+  echo $numerosTabuada[7] . " x " . $i . "= " . ($numerosTabuada[7] * $i) . PHP_EOL;
+}
+
+echo "_________________________________" . PHP_EOL;
+
+for ($i = 0; $i <= 10; $i++){
+  echo $numerosTabuada[8] . " x " . $i . "= " . ($numerosTabuada[8] * $i) . PHP_EOL;
+}
+
+echo "_________________________________" . PHP_EOL;
+
+for ($i = 0; $i <= 10; $i++){
+  echo $numerosTabuada[9] . " x " . $i . "= " . ($numerosTabuada[9] * $i) . PHP_EOL;
+}
