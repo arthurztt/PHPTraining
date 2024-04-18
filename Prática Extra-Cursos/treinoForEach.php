60e353c903830838af87d9f94254013d05530d00
arthurztt <artkikk1@gmail.com>
2024-04-18 14:41:49 -0300

    Adicionando arquivo de treino foreach e alteração no arquivo do pedra papel e tesoura

---
+++ Prática Extra-Cursos/treinoForEach.php
@@ -0,0 +1,12 @@
+<?php 
+  $user = [
+    'name' => 'Arthur',
+    'email' => 'arthuremail@gmail.com',
+    'birthDay' => '08/07/2006'
+  ];
+  
+  foreach ($user as $infos){
+    echo "";
+  }
+
+?>
